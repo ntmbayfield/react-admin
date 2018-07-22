b0d8b77e9de5673013c6fda98b58a30de651943b
Gildas Garcia <gildas.garcia@gmail.com>
2018-07-22 10:44:11 +0200

    [WIP] Debounce calls to crudGetMatching
    Fixes #2004
    
    - [x] Implementation
    - [ ] Tests

--- packages/ra-core/src/sideEffect/admin.js
+++ packages/ra-core/src/sideEffect/admin.js
@@ -9,6 +9,7 @@ import redirection from './redirection';
 import accumulate from './accumulate';
 import refresh from './refresh';
 import undo from './undo';
+import debounce from './debounce';
 
 /**
  * @param {Object} dataProvider A Data Provider function
@@ -22,6 +23,7 @@ export default (dataProvider, authProvider, i18nProvider) =>
             fetch(dataProvider)(),
             error(),
             accumulate(),
+            debounce(),
             redirection(),
             refresh(),
             notification(),
